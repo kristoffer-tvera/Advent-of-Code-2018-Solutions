015e2954c73cd494d34d9fc3e9254089c9cb1f59
Kristoffer Tverå <Bluekristoffer@gmail.com>
2018-12-02 15:01:00 +0100

    Main console program prints the solutions. Commented out previous days.

--- AOC.2018/Program.cs
+++ AOC.2018/Program.cs
@@ -6,11 +6,13 @@ namespace AOC._2018
     {
         private static void Main(string[] args)
         {
-            var day1Part1 = Solutions.Day1Part1(DataImport.Day1Data());
-            Console.WriteLine($"Day 1, part 1: {day1Part1}");
-            
-            var day1Part2 = Solutions.Day1Part2(DataImport.Day1Data());
-            Console.WriteLine($"Day 1, part 2: {day1Part2}");
+//            Console.WriteLine($"Day 1, part 1: {Solutions.Day1Part1(DataImport.Day1Data())}");
+
+//            Console.WriteLine($"Day 1, part 2: {Solutions.Day1Part2(DataImport.Day1Data())}");
+
+            Console.WriteLine($"Day 2, part 1: {Solutions.Day2Part1(DataImport.Day2Data())}");
+
+            Console.WriteLine($"Day 2, part 2: {Solutions.Day2Part2(DataImport.Day2Data())}");
         }
     }
 }
