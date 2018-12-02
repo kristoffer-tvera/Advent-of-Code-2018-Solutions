97aac9f1f0c83e39ad60d4b13ebf28c01da6a3f2
Kristoffer Tverå <Bluekristoffer@gmail.com>
2018-12-02 15:00:10 +0100

    File import of data for day2

--- AOC.2018/DataImport.cs
+++ AOC.2018/DataImport.cs
@@ -5,14 +5,20 @@ namespace AOC._2018
 {
     public static class DataImport
     {
-
         public static IEnumerable<int> Day1Data()
         {
             var lines = System.IO.File.ReadAllLines(@"..\AOC.2018\Data\day1input.txt");
 
             var linesAsNumbers = lines.Select(int.Parse);
-            
+
             return linesAsNumbers;
+        }
+
+        public static IEnumerable<string> Day2Data()
+        {
+            var lines = System.IO.File.ReadAllLines(@"..\AOC.2018\Data\day2input.txt");
+
+            return lines;
         }
     }
 }
